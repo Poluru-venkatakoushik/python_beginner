bc01d62e4224e5a748f4b89e73719c7cf8203989
Poluru-venkatakoushik <62533604+Poluru-venkatakoushik@users.noreply.github.com>
2020-04-08 13:22:49 +0530

    Add files via upload

---
+++ random_password_generator_secure.py
@@ -0,0 +1,19 @@
+#Random password generator
+import random
+from pygame import mixer
+def repetition_allowed(n):
+    allowed
+def repetition_not_allowed(n):
+    allowed
+password_size=int(input("Enter the password's size you want to have (*min of 8 charectars) : "))
+allowed=input("Repetition allowed or not? (Y/N) :").upper()
+if(allowed=="Y"):
+    repetition_allowed(password_size)
+elif(allowed=="N"):
+    repetition_not_allowed(password_size)
+else:
+    mixer.init()
+    mixer.music.load(r'C:\Users\Hp\Downloads\Smoke Alarm-SoundBible.com-1551222038.wav')
+    mixer.music.play()
+    used_set=[]
+    used_set.
